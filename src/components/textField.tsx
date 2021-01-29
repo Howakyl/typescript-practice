8a772f771f4e136df0d08fb40e2a062e5ae653fe
Howakyl96 <howakyl@gmail.com>
2021-01-28 17:24:57 -0800

    ts with hooks

--- src/components/textField.tsx
+++ src/components/textField.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 
 interface Person {
     firstName: string;
@@ -13,11 +14,22 @@ interface Props {
     person?: Person;
 }
 
+interface TextNode {
+    text: string
+}
+
 export const TextField: React.FC<Props> = (props) => {
+
+    // const [count, setCount] = useState<number | null >(5);
+    // const [count, setCount] = useState<TextNode>({text: 'hi'})
+
+    // setCount({text: 'yo'});
+
     return (
         <div>
             <input />
             <p>My name is {props.person?.firstName} {props.person?.lastName}</p>
+            {/* <p>{count}</p> */}
         </div>
     );
 };
